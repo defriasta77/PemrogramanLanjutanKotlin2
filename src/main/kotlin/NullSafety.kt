d4f1d32d1cef0d4552058be90174ca31a8e452e8
DEFRIASTARIDO <defri.astarido77@gmail.com>
2024-09-17 12:07:47 +0700

    add NullSafety.kt

---
+++ src/main/kotlin/NullSafety.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>){
+    //Nullable types
+
+    var maybeNumber: Int? = 15
+    maybeNumber = null
+    println("maybeNumber: $maybeNumber ${maybeNumber?.hashCode()}")
+}
